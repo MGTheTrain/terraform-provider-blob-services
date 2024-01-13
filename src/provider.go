1ff89f22bbeb202527aa2376aea933ca0bfc0da5
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-13 19:03:56 +0100

    move and update exclusive provider.go

--- src/provider.go
+++ src/provider.go
@@ -7,6 +7,7 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
+			"mgtt_aws_s3_bucket":                resourceAwsS3Bucket(),
 			"mgtt_az_storage_account":           resourceAzStorageAccount(),
 			"mgtt_az_storage_account_container": resourceAzStorageAccountContainer(),
 		},
