84f0249cecd4b61b5cf2051cdff654d38e60d364
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-13 21:43:54 +0100

    add/update tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
 resource "mgtt_aws_s3_bucket" "this" {
-    name = "test"
+    name = "great"
+}
+
+resource "mgtt_az_storage_account" "this" {
+    name = "great"
+}
+
+resource "mgtt_az_storage_account_container" "this" {
+    name = "great"
 }
