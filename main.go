eb5861882aa583e6290a174bcf68e8bc285945be
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-13 22:51:23 +0100

    rename package

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	layers "github.com/MGTheTrain/terraform-provider-blob-services/mgtt"
+	"github.com/MGTheTrain/terraform-provider-blob-services/mgtt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -10,7 +10,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return layers.Provider()
+			return mgtt.Provider()
 		},
 	})
 }
--- mgtt/provider.go
+++ mgtt/provider.go
@@ -1,4 +1,4 @@
-package layers
+package mgtt
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
