e943cef118b89d3da99efd61da43b1338335aa32
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-14 19:24:59 +0100

    rename variables and update input argument naming conventions

--- api-testing/azure_storage_account/azure_storage_account_handler.go
+++ api-testing/azure_storage_account/azure_storage_account_handler.go
@@ -12,27 +12,27 @@ import (
 )
 
 var (
-	subscriptionID      string
-	resourceGroupName   string
-	accountName         string
-	accessToken         string
-	requestBodyJsonPath string
+	subscriptionID    string
+	resourceGroupName string
+	accountName       string
+	accessToken       string
+	reqBodyJsonFile   string
 )
 
 func main() {
 	var rootCmd = &cobra.Command{Use: "azure_storage_account_handler"}
 
-	rootCmd.PersistentFlags().StringVarP(&subscriptionID, "subscriptionID", "s", "", "Your Azure subscription ID")
-	rootCmd.PersistentFlags().StringVarP(&resourceGroupName, "resourceGroupName", "g", "", "Your Azure resource group name")
-	rootCmd.PersistentFlags().StringVarP(&accountName, "accountName", "a", "", "Your Azure Storage account name")
-	rootCmd.PersistentFlags().StringVarP(&accessToken, "accessToken", "t", "", "Your Bearer access token")
-	rootCmd.PersistentFlags().StringVarP(&requestBodyJsonPath, "requestBodyJsonPath", "r", "", "The request body json path")
+	rootCmd.PersistentFlags().StringVarP(&subscriptionID, "subscription_id", "s", "", "Your Azure subscription ID")
+	rootCmd.PersistentFlags().StringVarP(&resourceGroupName, "resource_group_name", "g", "", "Your Azure resource group name")
+	rootCmd.PersistentFlags().StringVarP(&accountName, "account_name", "a", "", "Your Azure Storage account name")
+	rootCmd.PersistentFlags().StringVarP(&accessToken, "access_token", "t", "", "Your Bearer access token")
+	rootCmd.PersistentFlags().StringVarP(&reqBodyJsonFile, "req_body_json_file", "r", "", "The request body json file. E.g. assets/azure_storage_account_request_body.json")
 
 	rootCmd.AddCommand(&cobra.Command{
 		Use:   "manage",
 		Short: "Manage Azure Storage Account",
 		Run: func(cmd *cobra.Command, args []string) {
-			handleAzureStorageAccount(subscriptionID, resourceGroupName, accountName, accessToken, requestBodyJsonPath)
+			handleAzureStorageAccount(subscriptionID, resourceGroupName, accountName, accessToken, reqBodyJsonFile)
 		},
 	})
 
@@ -43,29 +43,30 @@ func main() {
 }
 
 // handleAzureStorageAccount handles the Azure Storage Account management.
-func handleAzureStorageAccount(subscriptionID, resourceGroupName, accountName, accessToken, requestBodyJsonPath string) {
+func handleAzureStorageAccount(subscriptionID, resourceGroupName, accountName, accessToken, reqBodyJsonFile string) {
 	fmt.Printf("Subscription ID: %s\n", subscriptionID)
 	fmt.Printf("Resource Group Name: %s\n", resourceGroupName)
 	fmt.Printf("Storage Account Name: %s\n", accountName)
 	fmt.Printf("Access Token: %s\n", accessToken)
+	fmt.Printf("Json Request Body File: %s\n", reqBodyJsonFile)
 
-	if subscriptionID == "" || resourceGroupName == "" || accountName == "" || accessToken == "" {
-		fmt.Println("Usage: azure_storage_account_handler manage -s <subscription_id> -g <resource_group_name> -a <storage_account_name> -t <bearer_access_token>")
+	if subscriptionID == "" || resourceGroupName == "" || accountName == "" || accessToken == "" || reqBodyJsonFile == "" {
+		fmt.Println("Usage: azure_storage_account_handler manage -s <subscription_id> -g <resource_group_name> -a <storage_account_name> -t <bearer_access_token> -r <req_body_json_file>")
 		return
 	}
 
-	err := submitHTTPRequest(subscriptionID, resourceGroupName, accountName, accessToken, requestBodyJsonPath)
+	err := submitHTTPRequest(subscriptionID, resourceGroupName, accountName, accessToken, reqBodyJsonFile)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 }
 
 // submitHTTPRequest submits an HTTP request to Azure Storage Account.
-func submitHTTPRequest(subscriptionID, resourceGroupName, accountName, accessToken, requestBodyJsonPath string) error {
+func submitHTTPRequest(subscriptionID, resourceGroupName, accountName, accessToken, reqBodyJsonFile string) error {
 	url := fmt.Sprintf("https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Storage/storageAccounts/%s?api-version=2018-02-01",
 		subscriptionID, resourceGroupName, accountName)
 
-	jsonFile, err := os.Open(requestBodyJsonPath) // e.g. assets/azure_storage_account_request_body.json
+	jsonFile, err := os.Open(reqBodyJsonFile)
 	if err != nil {
 		return fmt.Errorf("Error opening JSON file:", err)
 	}
