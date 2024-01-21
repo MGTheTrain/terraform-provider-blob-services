642fa5a530477cac93567682ecc7d44d17bbeeb5
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-21 18:39:45 +0100

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,27 @@ resource "mgtt_azurerm_rg" "this" {
 }
 
 resource "mgtt_azurerm_storage_account" "this" {
-    name = "great"
+    name                 = "uniqacc2400"
+    location             = "West Europe"
+    resource_group_name  = "West Europe"
+    
+    sku {
+        name = "Standard_LRS"
+        tier = "Standard"
+    }
+
+    kind = "StorageV2"
+
+    properties {
+        key_policy {
+            key_expiration_period_in_days = 20
+        }
+    }
 }
+
 
 resource "mgtt_azurerm_storage_account_container" "this" {
     name = "great"
+    location = "West Europe"
+    resource_group_name = "West Europe"
 }
