4adef5d8076aff1d74d3e5392185bbb9961eb4ec
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-22 21:01:36 +0100

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,31 @@
-resource "mgtt_aws_s3_bucket" "this" {
-    name = "great"
-}
+# resource "mgtt_aws_s3_bucket" "this" {
+#     name = "great"
+# }
 
 resource "mgtt_azurerm_rg" "this" {
-    name = "great"
+    name = "rg-test-5000"
     location = "West Europe"
 }
 
 resource "mgtt_azurerm_storage_account" "this" {
     name                 = "uniqacc2400"
-    location             = "West Europe"
-    resource_group_name  = "West Europe"
-    
-    sku {
-        name = "Standard_LRS"
-        tier = "Standard"
-    }
-
+    location             = mgtt_azurerm_rg.this.location
+    resource_group_name  = mgtt_azurerm_rg.this.name
     kind = "StorageV2"
+    
 
-    properties {
-        key_policy {
-            key_expiration_period_in_days = 20
-        }
-    }
+    sku_name = "Standard_LRS"
+    sku_tier = "Standard"
+
+    # sku {
+    #     name = "Standard_LRS"
+    #     tier = "Standard"
+    # }
 }
 
 
-resource "mgtt_azurerm_storage_account_container" "this" {
-    name = "great"
-    location = "West Europe"
-    resource_group_name = "West Europe"
-}
+# resource "mgtt_azurerm_storage_account_container" "this" {
+#     name = "great"
+#     location = "West Europe"
+#     resource_group_name = "West Europe"
+# }
