fbaacd042c46607fed9e446f12e50c86ee2a48c1
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-22 20:52:12 +0100

    update functions of storage account resource

--- mgtt/resource_mgtt_azurerm_storage_account.go
+++ mgtt/resource_mgtt_azurerm_storage_account.go
@@ -23,29 +23,6 @@ func skuSchema() *schema.Resource {
 	}
 }
 
-func propertiesSchema() *schema.Resource {
-	return &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"key_policy": &schema.Schema{
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem:     keyPolicySchema(),
-			},
-		},
-	}
-}
-
-func keyPolicySchema() *schema.Resource {
-	return &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"key_expiration_period_in_days": &schema.Schema{
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-		},
-	}
-}
-
 func resourceMgttAzurermStorageAccount() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceMgttAzurermStorageAccountCreate,
@@ -66,15 +43,18 @@ func resourceMgttAzurermStorageAccount() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"sku": &schema.Schema{
-				Type:     schema.TypeSet,
-				Elem:     skuSchema(),
+			// "sku": &schema.Schema{
+			// 	Type:     schema.TypeSet,
+			// 	Elem:     skuSchema(),
+			// 	Required: true,
+			// },
+			"sku_name": &schema.Schema{
+				Type:     schema.TypeString,
 				Required: true,
 			},
-			"properties": &schema.Schema{
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem:     propertiesSchema(),
+			"sku_tier": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"kind": &schema.Schema{
 				Type:     schema.TypeString,
@@ -89,49 +69,8 @@ func resourceMgttAzurermStorageAccountCreate(d *schema.ResourceData, m interface
 	resourceGroupName := d.Get("resource_group_name").(string)
 	location := d.Get("location").(string)
 	kind := d.Get("kind").(string)
-
-	skuSet := d.Get("sku").(*schema.Set)
-	skuName := ""
-	skuTier := ""
-	skuList := skuSet.List()
-	if len(skuList) > 0 {
-		skuMap := skuList[0].(map[string]interface{})
-		skuName = skuMap["name"].(string)
-		skuTier = skuMap["tier"].(string)
-		fmt.Printf("SKU Name: %s, SKU Tier: %s\n", skuName, skuTier)
-	}
-
-	propertiesSet := d.Get("properties").(*schema.Set)
-	keyExpirationPeriodInDays := 0
-	propertiesList := propertiesSet.List()
-	if len(propertiesList) > 0 {
-		propertiesMap := propertiesList[0].(map[string]interface{})
-		keyPolicySet := propertiesMap["key_policy"].(*schema.Set)
-		keyPolicyList := keyPolicySet.List()
-		if len(keyPolicyList) > 0 {
-			keyPolicyMap := keyPolicyList[0].(map[string]interface{})
-			keyExpirationPeriodInDays = keyPolicyMap["key_expiration_period_in_days"].(int)
-		}
-	}
-
-	if err := d.Set("name", name); err != nil {
-		return err
-	}
-	if err := d.Set("resource_group_name", resourceGroupName); err != nil {
-		return err
-	}
-	if err := d.Set("location", location); err != nil {
-		return err
-	}
-	if err := d.Set("sku", map[string]interface{}{"name": skuName, "tier": skuTier}); err != nil {
-		return err
-	}
-	if err := d.Set("kind", kind); err != nil {
-		return err
-	}
-	if err := d.Set("properties", map[string]interface{}{"keyPolicy": map[string]interface{}{"keyExpirationPeriodInDays": keyExpirationPeriodInDays}}); err != nil {
-		return err
-	}
+	skuName := d.Get("sku_name").(string)
+	skuTier := d.Get("sku_tier").(string)
 
 	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
 	accessToken := os.Getenv("AZURE_ACCESS_TOKEN")
@@ -144,11 +83,6 @@ func resourceMgttAzurermStorageAccountCreate(d *schema.ResourceData, m interface
 		},
 		"kind":     kind,
 		"location": location,
-		"properties": map[string]interface{}{
-			"keyPolicy": map[string]interface{}{
-				"keyExpirationPeriodInDays": keyExpirationPeriodInDays,
-			},
-		},
 	}
 
 	jsonString, err := ConvertMapToJSON(createRequestBody)
@@ -156,7 +90,7 @@ func resourceMgttAzurermStorageAccountCreate(d *schema.ResourceData, m interface
 		return fmt.Errorf("Error converting map to JSON: %s", err)
 	}
 
-	err = azureStorageAccountHandler.CreateAzureStorageAccount(resourceGroupName, name, jsonString)
+	err = azureStorageAccountHandler.CreateStorageAccount(resourceGroupName, name, jsonString)
 
 	if err != nil {
 		return err
@@ -164,6 +98,26 @@ func resourceMgttAzurermStorageAccountCreate(d *schema.ResourceData, m interface
 
 	id := uuid.New()
 	d.SetId(id.String())
+
+	if err := d.Set("name", name); err != nil {
+		return err
+	}
+	if err := d.Set("resource_group_name", resourceGroupName); err != nil {
+		return err
+	}
+	if err := d.Set("location", location); err != nil {
+		return err
+	}
+	if err := d.Set("sku_name", skuName); err != nil {
+		return err
+	}
+	if err := d.Set("sku_tier", skuTier); err != nil {
+		return err
+	}
+	if err := d.Set("kind", kind); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -175,7 +129,7 @@ func resourceMgttAzurermStorageAccountRead(d *schema.ResourceData, m interface{}
 	accessToken := os.Getenv("AZURE_ACCESS_TOKEN")
 	azureStorageAccountHandler := NewAzureStorageAccountHandler(subscriptionID, accessToken)
 
-	err := azureStorageAccountHandler.GetAzureStorageAccount(resourceGroupName, name)
+	err := azureStorageAccountHandler.GetStorageAccount(resourceGroupName, name)
 
 	if err != nil {
 		return err
@@ -188,29 +142,33 @@ func resourceMgttAzurermStorageAccountUpdate(d *schema.ResourceData, m interface
 	resourceGroupName := d.Get("resource_group_name").(string)
 	location := d.Get("location").(string)
 	kind := d.Get("kind").(string)
+	skuName := d.Get("sku_name").(string)
+	skuTier := d.Get("sku_tier").(string)
 
-	skuSet := d.Get("sku").(*schema.Set)
-	skuName := ""
-	skuTier := ""
-	skuList := skuSet.List()
-	if len(skuList) > 0 {
-		skuMap := skuList[0].(map[string]interface{})
-		skuName = skuMap["name"].(string)
-		skuTier = skuMap["tier"].(string)
-		fmt.Printf("SKU Name: %s, SKU Tier: %s\n", skuName, skuTier)
+	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	accessToken := os.Getenv("AZURE_ACCESS_TOKEN")
+	azureStorageAccountHandler := NewAzureStorageAccountHandler(subscriptionID, accessToken)
+
+	azureStorageAccountHandler.DeleteStorageAccount(resourceGroupName, name)
+
+	createRequestBody := map[string]interface{}{
+		"sku": map[string]interface{}{
+			"name": skuName,
+			"tier": skuTier,
+		},
+		"kind":     kind,
+		"location": location,
 	}
 
-	propertiesSet := d.Get("properties").(*schema.Set)
-	keyExpirationPeriodInDays := 0
-	propertiesList := propertiesSet.List()
-	if len(propertiesList) > 0 {
-		propertiesMap := propertiesList[0].(map[string]interface{})
-		keyPolicySet := propertiesMap["key_policy"].(*schema.Set)
-		keyPolicyList := keyPolicySet.List()
-		if len(keyPolicyList) > 0 {
-			keyPolicyMap := keyPolicyList[0].(map[string]interface{})
-			keyExpirationPeriodInDays = keyPolicyMap["key_expiration_period_in_days"].(int)
-		}
+	jsonString, err := ConvertMapToJSON(createRequestBody)
+	if err != nil {
+		return fmt.Errorf("Error converting map to JSON: %s", err)
+	}
+
+	err = azureStorageAccountHandler.CreateStorageAccount(resourceGroupName, name, jsonString)
+
+	if err != nil {
+		return err
 	}
 
 	if err := d.Set("name", name); err != nil {
@@ -222,38 +180,16 @@ func resourceMgttAzurermStorageAccountUpdate(d *schema.ResourceData, m interface
 	if err := d.Set("location", location); err != nil {
 		return err
 	}
-	if err := d.Set("sku", map[string]interface{}{"name": skuName, "tier": skuTier}); err != nil {
+	if err := d.Set("sku_name", skuName); err != nil {
+		return err
+	}
+	if err := d.Set("sku_tier", skuTier); err != nil {
 		return err
 	}
 	if err := d.Set("kind", kind); err != nil {
 		return err
 	}
-	if err := d.Set("properties", map[string]interface{}{"keyPolicy": map[string]interface{}{"keyExpirationPeriodInDays": keyExpirationPeriodInDays}}); err != nil {
-		return err
-	}
 
-	subscriptionID := os.Getenv("AZURE_SUBSCRIPTION_ID")
-	accessToken := os.Getenv("AZURE_ACCESS_TOKEN")
-	azureStorageAccountHandler := NewAzureStorageAccountHandler(subscriptionID, accessToken)
-
-	createRequestBody := map[string]interface{}{
-		"properties": map[string]interface{}{
-			"keyPolicy": map[string]interface{}{
-				"keyExpirationPeriodInDays": keyExpirationPeriodInDays,
-			},
-		},
-	}
-
-	jsonString, err := ConvertMapToJSON(createRequestBody)
-	if err != nil {
-		return fmt.Errorf("Error converting map to JSON: %s", err)
-	}
-
-	err = azureStorageAccountHandler.UpdateAzureStorageAccount(resourceGroupName, name, jsonString)
-
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
@@ -265,7 +201,7 @@ func resourceMgttAzurermStorageAccountDelete(d *schema.ResourceData, m interface
 	accessToken := os.Getenv("AZURE_ACCESS_TOKEN")
 	azureStorageAccountHandler := NewAzureStorageAccountHandler(subscriptionID, accessToken)
 
-	err := azureStorageAccountHandler.DeleteAzureStorageAccount(resourceGroupName, name)
+	err := azureStorageAccountHandler.DeleteStorageAccount(resourceGroupName, name)
 
 	if err != nil {
 		return err
