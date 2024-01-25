bb4fb87a71579260521de7ed679f79d807e9a61d
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-25 22:31:27 +0100

    consider the http request to sign as input parameter

--- api-testing/aws_s3_bucket/aws_s3_bucket_handler.go
+++ api-testing/aws_s3_bucket/aws_s3_bucket_handler.go
@@ -9,9 +9,9 @@ import (
 	"time"
 )
 
-func calculate_hmac_sha1_signature(secretKey string) string {
+func calculate_hmac_sha1_signature(secretKey, request string) string {
 	dateTime := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 -0700")
-	stringToSign := fmt.Sprintf("GET\n\n\n%s\n/awsexamplebucket1/photos/puppy.jpg", dateTime)
+	stringToSign := fmt.Sprintf(request, dateTime)
 	h := hmac.New(sha1.New, []byte(secretKey))
 	h.Write([]byte(stringToSign))
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
@@ -22,7 +22,8 @@ func main() {
 	accessKey := os.Getenv("ACCESS_KEY")
 	secretKey := os.Getenv("SECRET_KEY")
 
-	signature := calculate_hmac_sha1_signature(secretKey)
+	request := "GET\n\n\n%s\n/awsexamplebucket1/photos/puppy.jpg" // HTTP request to be signed
+	signature := calculate_hmac_sha1_signature(secretKey, request)
 
 	fmt.Printf("AWS %s:%s\n", accessKey, signature)
 }
